97b893ca243182d1246d51c070576df6fc38b4bf
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-21 19:54:41 -0700

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,4 +1,4 @@
-$mcPath = "$env:APPDATA\.minecraft"
+$mcPath = "$env:APPDATA\minecraft"
 $manifest = Invoke-RestMethod -Uri "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json"
 $latestReleaseId = $manifest.latest.release
 $latestReleaseUrl = ($manifest.versions | Where-Object { $_.id -eq $latestReleaseId }).url
