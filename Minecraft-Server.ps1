31e748b28ec7994a42ee0985e2d7dcace85a5521
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-22 10:14:15 -0700

    Update Minecraft-Server.ps1

--- Minecraft-Server.ps1
+++ Minecraft-Server.ps1
@@ -4,8 +4,10 @@ New-Item -ItemType Directory -Path $folderPath -Force
 $jarUrl = "https://piston-data.mojang.com/v1/objects/6bce4ef400e4efaa63a13d5e6f6b500be969ef81/server.jar"
 $jarPath = Join-Path $folderPath "server.jar"
 
-$webClient = New-Object System.Net.WebClient
-$webClient.DownloadFile($jarUrl, $jarPath)
+if (-Not (Test-Path $jarPath)) {
+    $webClient = New-Object System.Net.WebClient
+    $webClient.DownloadFile($jarUrl, $jarPath)
+}
 
 $eulaPath = Join-Path $folderPath "eula.txt"
 Set-Content -Path $eulaPath -Value "eula=true"
