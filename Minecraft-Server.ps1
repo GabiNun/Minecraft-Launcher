309271eea871d8c18a699ca28e6e30fca7df8e50
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-22 10:13:34 -0700

    Update Minecraft-Server.ps1

--- Minecraft-Server.ps1
+++ Minecraft-Server.ps1
@@ -1,10 +1,14 @@
-$jarUrl = "https://piston-data.mojang.com/v1/objects/6bce4ef400e4efaa63a13d5e6f6b500be969ef81/server.jar"
 $folderPath = Join-Path $env:APPDATA "Minecraft Server"
-$jarPath = Join-Path $folderPath "server.jar"
-$eulaPath = Join-Path $folderPath "eula.txt"
-
 New-Item -ItemType Directory -Path $folderPath -Force
-Invoke-WebRequest -Uri $jarUrl -OutFile $jarPath
+
+$jarUrl = "https://piston-data.mojang.com/v1/objects/6bce4ef400e4efaa63a13d5e6f6b500be969ef81/server.jar"
+$jarPath = Join-Path $folderPath "server.jar"
+
+$webClient = New-Object System.Net.WebClient
+$webClient.DownloadFile($jarUrl, $jarPath)
+
+$eulaPath = Join-Path $folderPath "eula.txt"
 Set-Content -Path $eulaPath -Value "eula=true"
+
 Set-Location $folderPath
-& java -Xmx8192M -jar server.jar nogui
+& java -Xmx8192M -jar "server.jar" nogui
