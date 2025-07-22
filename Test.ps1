cf20b82cb3c4192096dc6e9de7e33efd6fe7450d
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-21 21:38:22 -0700

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,6 +1,15 @@
-$manifest = irm piston-meta.mojang.com/mc/game/version_manifest_v2.json
-$latestReleaseUrl = ($manifest.versions | ? id -eq $manifest.latest.release).url
+$manifest = irm "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json"
+$latestReleaseUrl = ($manifest.versions | Where-Object { $_.id -eq $manifest.latest.release }).url
+$assetIndex = irm (irm $latestReleaseUrl).assetIndex.url
 
-$versionData = irm $latestReleaseUrl
-New-Item -ItemType Directory $env:AppData\minecraft -Force
-irm $versionData.downloads.client.url -OutFile $env:APPDATA\minecraft\client.jar
+foreach ($entry in $assetIndex.objects.psobject.Properties) {
+    $path = $entry.Name
+    if ($path -like "minecraft/sounds/*" -or $path -eq "minecraft/sounds.json") { continue }
+    if ($path -like "minecraft/lang/*" -and $path -ne "minecraft/lang/en_us.json") { continue }
+    $hash = $entry.Value.hash
+    $subDir = $hash.Substring(0, 2)
+    $url = "https://resources.download.minecraft.net/$subDir/$hash"
+    $outPath = Join-Path -Path "assets" -ChildPath $path
+    New-Item -ItemType Directory -Path (Split-Path $outPath) -Force | Out-Null
+    Invoke-WebRequest -Uri $url -OutFile $outPath
+}
