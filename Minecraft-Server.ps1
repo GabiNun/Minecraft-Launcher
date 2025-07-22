fd40cd5865e50dc9685a5b91b6c033da2229695c
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-22 10:44:43 -0700

    Update Minecraft-Server.ps1

--- Minecraft-Server.ps1
+++ Minecraft-Server.ps1
@@ -4,7 +4,12 @@ New-Item -ItemType Directory "$env:APPDATA\Minecraft Server" -Force
 Set-Content -Path "$env:APPDATA\Minecraft Server\eula.txt" -Value "eula=true"
 
 if (-Not (Test-Path "$env:APPDATA\Minecraft Server\server.jar")) {
-    Invoke-WebRequest -Uri "https://piston-data.mojang.com/v1/objects/6bce4ef400e4efaa63a13d5e6f6b500be969ef81/server.jar" -OutFile "$env:APPDATA\Minecraft Server\server.jar"
+    $url = if ((Read-Host "Choose server type (default/paper)").ToLower() -eq "paper") {
+        "https://fill-data.papermc.io/v1/objects/9457d1279efcc2094e818cacb2f17670d9479e5f6b4ea2517eb93a6a3face51f/paper-1.21.8-11.jar"
+    } else {
+        "https://piston-data.mojang.com/v1/objects/6bce4ef400e4efaa63a13d5e6f6b500be969ef81/server.jar"
+    }
+    Invoke-WebRequest $url -OutFile "$env:APPDATA\Minecraft Server\server.jar"
 }
 
 Set-Location "$env:APPDATA\Minecraft Server"
