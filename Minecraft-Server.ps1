1cb1ed9b8a2d0e7a7dee7ac6c9e41aac93adb8cf
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-22 10:17:32 -0700

    Update Minecraft-Server.ps1

--- Minecraft-Server.ps1
+++ Minecraft-Server.ps1
@@ -5,8 +5,15 @@ $jarUrl = "https://piston-data.mojang.com/v1/objects/6bce4ef400e4efaa63a13d5e6f6
 $jarPath = Join-Path $folderPath "server.jar"
 
 if (-Not (Test-Path $jarPath)) {
-    $webClient = New-Object System.Net.WebClient
-    $webClient.DownloadFile($jarUrl, $jarPath)
+    Add-Type -AssemblyName System.Net.Http
+    $httpClient = [System.Net.Http.HttpClient]::new()
+    $response = $httpClient.GetAsync($jarUrl, [System.Net.Http.HttpCompletionOption]::ResponseHeadersRead).Result
+    $stream = $response.Content.ReadAsStreamAsync().Result
+    $fileStream = [System.IO.File]::Create($jarPath)
+    $stream.CopyTo($fileStream)
+    $fileStream.Close()
+    $stream.Close()
+    $httpClient.Dispose()
 }
 
 $eulaPath = Join-Path $folderPath "eula.txt"
