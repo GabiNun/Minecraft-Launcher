2af539c0ec4794dfeaf55f3ae6c21c4951fec437
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-22 10:20:38 -0700

    Update Minecraft-Server.ps1

--- Minecraft-Server.ps1
+++ Minecraft-Server.ps1
@@ -1,23 +1,10 @@
-$folderPath = Join-Path $env:APPDATA "Minecraft Server"
-New-Item -ItemType Directory -Path $folderPath -Force
+$ProgressPreference = 'SilentlyContinue'
 
-$jarUrl = "https://piston-data.mojang.com/v1/objects/6bce4ef400e4efaa63a13d5e6f6b500be969ef81/server.jar"
-$jarPath = Join-Path $folderPath "server.jar"
+New-Item -ItemType Directory -Path "$env:APPDATA\Minecraft Server" -Force
+Set-Content -Path "$env:APPDATA\Minecraft Server\eula.txt" -Value "eula=true"
 
-if (-Not (Test-Path $jarPath)) {
-    Add-Type -AssemblyName System.Net.Http
-    $httpClient = [System.Net.Http.HttpClient]::new()
-    $response = $httpClient.GetAsync($jarUrl, [System.Net.Http.HttpCompletionOption]::ResponseHeadersRead).Result
-    $stream = $response.Content.ReadAsStreamAsync().Result
-    $fileStream = [System.IO.File]::Create($jarPath)
-    $stream.CopyTo($fileStream)
-    $fileStream.Close()
-    $stream.Close()
-    $httpClient.Dispose()
+if (-Not (Test-Path "$env:APPDATA\Minecraft Server\server.jar")) {
+    Invoke-WebRequest -Uri "https://piston-data.mojang.com/v1/objects/6bce4ef400e4efaa63a13d5e6f6b500be969ef81/server.jar" -OutFile "$env:APPDATA\Minecraft Server\server.jar"
 }
 
-$eulaPath = Join-Path $folderPath "eula.txt"
-Set-Content -Path $eulaPath -Value "eula=true"
-
-Set-Location $folderPath
-& java -Xmx8192M -jar "server.jar" nogui
+& java -Xmx8192M -jar "$env:APPDATA\Minecraft Server\server.jar" nogui
