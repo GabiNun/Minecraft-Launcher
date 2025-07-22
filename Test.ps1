98d2a01522099043313d2714b254763e0ab778a6
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-21 21:26:50 -0700

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -2,5 +2,5 @@ $manifest = irm piston-meta.mojang.com/mc/game/version_manifest_v2.json
 $latestReleaseUrl = ($manifest.versions | ? id -eq $manifest.latest.release).url
 
 $versionData = irm $latestReleaseUrl
-New-Item $env:AppData\minecraft -Force
+New-Item -ItemType Directory $env:AppData\minecraft -Force
 irm $versionData.downloads.client.url -OutFile $env:APPDATA\minecraft\client.jar
