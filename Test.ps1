576efd6e989f3cdfff6ab4d25ceee58f6a370979
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-21 21:42:46 -0700

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,15 +1,2 @@
 $manifest = irm "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json"
 $latestReleaseUrl = ($manifest.versions | Where-Object { $_.id -eq $manifest.latest.release }).url
-$assetIndex = irm (irm $latestReleaseUrl).assetIndex.url
-
-foreach ($entry in $assetIndex.objects.psobject.Properties) {
-    $path = $entry.Name
-    if ($path -like "minecraft/sounds/*" -or $path -eq "minecraft/sounds.json") { continue }
-    if ($path -like "minecraft/lang/*" -and $path -ne "minecraft/lang/en_us.json") { continue }
-    $hash = $entry.Value.hash
-    $subDir = $hash.Substring(0, 2)
-    $url = "https://resources.download.minecraft.net/$subDir/$hash"
-    $outPath = Join-Path -Path "assets" -ChildPath $path
-    New-Item -ItemType Directory -Path (Split-Path $outPath) -Force | Out-Null
-    Invoke-WebRequest -Uri $url -OutFile $outPath
-}
