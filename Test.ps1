0e9721fb240a72cd033151b5721dbe61c5684828
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-21 20:17:24 -0700

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,5 +1,4 @@
 $manifest = Invoke-RestMethod -Uri "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json"
-$latestReleaseId = $manifest.latest.release
-$latestReleaseUrl = ($manifest.versions | Where-Object { $_.id -eq $latestReleaseId }).url
+$latestReleaseUrl = ($manifest.versions | Where-Object { $_.id -eq $manifest.latest.release }).url
 
 $versionData = Invoke-RestMethod -Uri $latestReleaseUrl
