3aae9029af06e10ebfb22230c1d6cce21a598d3e
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-21 21:21:54 -0700

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,2 +1,5 @@
 $manifest = irm piston-meta.mojang.com/mc/game/version_manifest_v2.json
 $latestReleaseUrl = ($manifest.versions | ? id -eq $manifest.latest.release).url
+
+$versionData = irm $latestReleaseUrl
+irm $versionData.downloads.client.url -OutFile client.jar
