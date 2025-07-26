a18e54eab0d8d34b9378c39bc0759d40330df93b
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-25 23:38:17 -0700

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -2,6 +2,8 @@ $manifest = irm launchermeta.mojang.com/mc/game/version_manifest.json
 $latestReleaseUrl = ($manifest.versions | ? id -eq $manifest.latest.release).url
 $latestReleaseData = irm $latestReleaseUrl
 
+New-Item "$env:APPDATA\Minecraft Server" -ItemType Directory -Force | Out-Null
+
 $filePath = "$env:APPDATA\Minecraft Server\server.jar"
 if (-not (Test-Path $filePath)) {
     irm $latestReleaseData.downloads.server.url -OutFile $filePath
