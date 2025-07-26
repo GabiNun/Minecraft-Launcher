87e4e7a3d8b8524bc8244cb61d4d8256975c8578
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-25 23:41:47 -0700

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -9,3 +9,11 @@ $filePath = "$env:APPDATA\Minecraft Server\server.jar"
 if (-not (Test-Path $filePath)) {
     irm $latestReleaseData.downloads.server.url -OutFile $filePath
 }
+
+$eulaPath = "$env:APPDATA\Minecraft Server\eula.txt"
+if (-not (Test-Path $eulaPath)) {
+    "eula=true" | Out-File -Encoding ASCII $eulaPath
+}
+
+Set-Location  "$env:APPDATA\Minecraft Server"
+& java -jar server.jar nogui
