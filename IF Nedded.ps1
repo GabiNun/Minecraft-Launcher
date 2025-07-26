2c3f4ef35424e9a5d2a86572c695dd59e6c8cdde
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-25 21:53:28 -0700

    Create IF Nedded.ps1

---
+++ IF Nedded.ps1
@@ -0,0 +1,12 @@
+foreach ($lib in $latestReleaseData.libraries) {
+    if ($lib.downloads -and $lib.downloads.artifact) {
+        $filePath = Join-Path $env:APPDATA ".minecraft\libraries\$($lib.downloads.artifact.path)"
+        if (-not (Test-Path (Split-Path $filePath))) {
+            New-Item -ItemType Directory -Path (Split-Path $filePath) -Force | Out-Null
+        }
+        if (-not (Test-Path $filePath)) {
+            irm $lib.downloads.artifact.url -OutFile $filePath
+            Write-Host "Downloaded $($lib.downloads.artifact.path)"
+        }
+    }
+}
