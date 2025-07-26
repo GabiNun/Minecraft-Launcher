b51d3459815f859ca2c7cd5a308628c935d19026
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-25 23:49:28 -0700

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,6 +1,5 @@
 $manifest = irm launchermeta.mojang.com/mc/game/version_manifest.json
 $latestReleaseUrl = ($manifest.versions | ? id -eq $manifest.latest.release).url
-$latestRelease = irm $latestReleaseUrl
 
 New-Item "$env:APPDATA\Minecraft Server" -ItemType Directory -Force | Out-Null
 Set-Content "$env:APPDATA\Minecraft Server\eula.txt" -Value "eula=true"
@@ -8,7 +7,7 @@ $ProgressPreference = 'SilentlyContinue'
 
 $filePath = "$env:APPDATA\Minecraft Server\server.jar"
 if (-not (Test-Path $filePath)) {
-    irm $latestRelease.downloads.server.url -OutFile $filePath
+    irm ((irm $latestReleaseUrl).downloads.server.url) -OutFile $filePath
 }
 
 Set-Location "$env:APPDATA\Minecraft Server"
