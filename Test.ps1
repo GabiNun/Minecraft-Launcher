c97d7dd67d9c1ff0742914780bb3f75900c40cf3
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-25 21:44:03 -0700

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -4,4 +4,5 @@ $ProgressPreference = 'SilentlyContinue'
 $manifest = irm launchermeta.mojang.com/mc/game/version_manifest.json
 $latestReleaseUrl = ($manifest.versions | ? id -eq $manifest.latest.release).url
 $latestReleaseData = irm $latestReleaseUrl
+
 irm $latestReleaseData.downloads.client.url -OutFile $env:APPDATA\.minecraft\client.jar
