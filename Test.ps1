7ea1bcb670a337e8be08c8f3164dbf34ddf4881e
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-25 21:52:33 -0700

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -8,14 +8,13 @@ $latestReleaseData = irm $latestReleaseUrl
 irm $latestReleaseData.downloads.client.url -OutFile $env:APPDATA\.minecraft\client.jar
 
 foreach ($lib in $latestReleaseData.libraries) {
-    if ($lib.downloads -and $lib.downloads.artifact) {
-        $filePath = Join-Path $env:APPDATA ".minecraft\libraries\$($lib.downloads.artifact.path)"
-        if (-not (Test-Path (Split-Path $filePath))) {
-            New-Item -ItemType Directory -Path (Split-Path $filePath) -Force | Out-Null
-        }
-        if (-not (Test-Path $filePath)) {
-            irm $lib.downloads.artifact.url -OutFile $filePath
-            Write-Host "Downloaded $($lib.downloads.artifact.path)"
+    if ($lib.downloads?.artifact) {
+        $path = $lib.downloads.artifact.path.ToLower()
+        $skip = $lib.rules -and ($lib.rules | Where-Object { $_.os -and ($_.os.name -match 'linux|macos|arm64') }) -or ($path -match 'linux|macos|arm64')
+        if (-not $skip) {
+            $file = Join-Path $env:APPDATA ".minecraft\libraries\$($lib.downloads.artifact.path)"
+            if (-not (Test-Path (Split-Path $file))) { New-Item -ItemType Directory -Path (Split-Path $file) -Force | Out-Null }
+            if (-not (Test-Path $file)) { irm $lib.downloads.artifact.url -OutFile $file; Write-Host "Downloaded $($lib.downloads.artifact.path)" }
         }
     }
 }
