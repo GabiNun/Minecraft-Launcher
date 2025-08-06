24c5e1eb4f89ab1506e943044badef0f8c951201
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-06 11:03:54 -0700

    Update Paper-Server.ps1

--- Paper-Server.ps1
+++ Paper-Server.ps1
@@ -1,1 +1,11 @@
+New-Item "$env:APPDATA\Minecraft Server" -ItemType Directory -Force | Out-Null
+Set-Content "$env:APPDATA\Minecraft Server\eula.txt" eula=true
+$ProgressPreference = 'SilentlyContinue'
 
+$filePath = "$env:APPDATA\Minecraft Server\server.jar"
+if (-not (Test-Path $filePath)) {
+    irm fill-data.papermc.io/v1/objects/fb73c7e310215016955617ab957022d9e1d47aeba206df3a98c5ecb43756527c/paper-1.21.8-25.jar -OutFile $filePath
+}
+
+Set-Location "$env:APPDATA\Minecraft Server"
+& java -jar server.jar nogui
