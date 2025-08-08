5ff5c77e6a259667abbe9077db4f45792bd2a557
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-08 10:02:23 -0700

    Update Minecraft-Server.ps1

--- Minecraft-Server.ps1
+++ Minecraft-Server.ps1
@@ -1,9 +1,9 @@
-$manifest = irm launchermeta.mojang.com/mc/game/version_manifest.json
-$latestReleaseUrl = ($manifest.versions | ? id -eq $manifest.latest.release).url
-
 New-Item "$env:APPDATA\Minecraft Server" -ItemType Directory -Force | Out-Null
 Set-Content "$env:APPDATA\Minecraft Server\eula.txt" eula=true
 $ProgressPreference = 'SilentlyContinue'
+
+$manifest = irm launchermeta.mojang.com/mc/game/version_manifest.json
+$latestReleaseUrl = ($manifest.versions | ? id -eq $manifest.latest.release).url
 
 $filePath = "$env:APPDATA\Minecraft Server\server.jar"
 if (-not (Test-Path $filePath)) {
