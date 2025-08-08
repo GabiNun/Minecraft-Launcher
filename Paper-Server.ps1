9e7789ba9208c49d03f570755aa97189f8d5b5f9
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-08 09:43:14 -0700

    Update Paper-Server.ps1

--- Paper-Server.ps1
+++ Paper-Server.ps1
@@ -4,7 +4,7 @@ $ProgressPreference = 'SilentlyContinue'
 
 $filePath = "$env:APPDATA\Minecraft Server\server.jar"
 if (-not (Test-Path $filePath)) {
-    irm fill-data.papermc.io/v1/objects/fb73c7e310215016955617ab957022d9e1d47aeba206df3a98c5ecb43756527c/paper-1.21.8-25.jar -OutFile $filePath
+    irm fill-data.papermc.io/v1/objects/e799bb4890668c23bdfcf8bb265d10813f9fadd1db5cddfc531e8e4b6f614347/paper-1.21.8-27.jar -OutFile $filePath
 }
 
 Set-Location "$env:APPDATA\Minecraft Server"
