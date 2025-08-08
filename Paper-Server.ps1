e627d9ddad839c1149a0215c2c73db81e5827a7f
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-08-08 09:57:26 -0700

    Update Paper-Server.ps1

--- Paper-Server.ps1
+++ Paper-Server.ps1
@@ -2,13 +2,13 @@ New-Item "$env:APPDATA\Minecraft Server" -ItemType Directory -Force | Out-Null
 Set-Content "$env:APPDATA\Minecraft Server\eula.txt" eula=true
 $ProgressPreference = 'SilentlyContinue'
 
-$latestVersion = (irm "https://api.papermc.io/v2/projects/paper").versions[-1]
-$latestBuild = (irm "https://api.papermc.io/v2/projects/paper/versions/$latestVersion").builds[-1]
-$downloadName = (irm "https://api.papermc.io/v2/projects/paper/versions/$latestVersion/builds/$latestBuild").downloads.application.name
+$latestVersion = (irm api.papermc.io/v2/projects/paper).versions[-1]
+$latestBuild = (irm api.papermc.io/v2/projects/paper/versions/$latestVersion).builds[-1]
+$downloadName = (irm api.papermc.io/v2/projects/paper/versions/$latestVersion/builds/$latestBuild).downloads.application.name
 
 $filePath = "$env:APPDATA\Minecraft Server\server.jar"
 if (-not (Test-Path $filePath)) {
-    irm api.papermc.io/v2/projects/paper/versions/$latestVersion/builds/$latestBuild/downloads/$downloadName -OutFile $filePath
+    irm api.papermc.io/v2/projects/paper/versions/$latestVersion/builds/$latestBuild/downloads/$downloadName -o $filePath
 }
 
 Set-Location "$env:APPDATA\Minecraft Server"
