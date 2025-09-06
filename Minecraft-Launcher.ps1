8e80acc4ab99b764ac017dc194a14ae5d8c78282
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-06 14:53:28 +0300

    Update Minecraft-Launcher.ps1

--- Minecraft-Launcher.ps1
+++ Minecraft-Launcher.ps1
@@ -27,7 +27,8 @@ if (-not (Test-Path $indexFilePath)) {
 
 foreach ($lib in $latestReleaseData.libraries) {
     if ($lib.downloads -and $lib.downloads.artifact) {
-        $skip = ($lib.rules -and ($lib.rules | Where-Object { $_.os -and ($_.os.name -match 'linux|macos|arm64') })) -or ($lib.downloads.artifact.path.ToLower() -match 'linux|macos|arm64')
+        $path = $lib.downloads.artifact.path.ToLower()
+        $skip = ($lib.rules -and ($lib.rules | Where-Object { $_.os -and ($_.os.name -match 'linux|macos|arm64') })) -or ($path -match 'linux|macos|arm64')
         if (-not $skip) {
             $file = Join-Path $env:APPDATA ".minecraft\libraries\$($lib.downloads.artifact.path)"
             if (-not (Test-Path (Split-Path $file))) { New-Item -ItemType Directory -Path (Split-Path $file) -Force | Out-Null }
