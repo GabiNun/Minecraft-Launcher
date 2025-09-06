07b61cc3312ee412dbe176ebf9821c27b064a8de
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-06 18:02:07 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,39 +1,53 @@
-# Microsoft Login Authentication for Minecraft Launcher
-
 function Get-Microsoft-Minecraft-Identity {
 
-    Start-Process "https://login.live.com/oauth20_authorize.srf?client_id=00000000402b5328&response_type=code&redirect_uri=https://login.live.com/oauth20_desktop.srf&scope=XboxLive.signin offline_access"
+    $clientId = "00000000402b5328"
+    $redirectUri = "https://login.live.com/oauth20_desktop.srf"
+    $scope = "XboxLive.signin offline_access"
+    $loginUrl = "https://login.live.com/oauth20_authorize.srf?client_id=$clientId&response_type=code&redirect_uri=$redirectUri&scope=$scope"
+
+    Start-Process $loginUrl
+
     $code = Read-Host "`nPaste in the value after code= and before &lc=1033)"
 
-    $access = (Invoke-RestMethod -Method Post -Uri "https://login.live.com/oauth20_token.srf" -Body @{
-        client_id="00000000402b5328"
-        code=$code
-        grant_type="authorization_code"
-        redirect_uri="https://login.live.com/oauth20_desktop.srf"
-    }).access_token
+    $tokenResponse = Invoke-RestMethod -Method Post -Uri "https://login.live.com/oauth20_token.srf" -Body @{
+        client_id     = $clientId
+        code          = $code
+        grant_type    = "authorization_code"
+        redirect_uri  = $redirectUri
+    }
 
-    $xblToken = (Invoke-RestMethod -Method Post -Uri "https://user.auth.xboxlive.com/user/authenticate" -Headers @{ "Content-Type"="application/json" } -Body (@{
+    $accessToken = $tokenResponse.access_token
+
+    $xblBody = @{
         Properties = @{
-            AuthMethod="RPS"
-            SiteName="user.auth.xboxlive.com"
-            RpsTicket="d=$access"
+            AuthMethod = "RPS"
+            SiteName   = "user.auth.xboxlive.com"
+            RpsTicket  = "d=$accessToken"
         }
-        RelyingParty="http://auth.xboxlive.com"
-        TokenType="JWT"
-    } | ConvertTo-Json)).Token
+        RelyingParty = "http://auth.xboxlive.com"
+        TokenType    = "JWT"
+    } | ConvertTo-Json
 
-    $xsts = Invoke-RestMethod -Method Post -Uri "https://xsts.auth.xboxlive.com/xsts/authorize" -Headers @{ "Content-Type"="application/json" } -Body (@{
+    $xblToken = (Invoke-RestMethod -Method Post -Uri "https://user.auth.xboxlive.com/user/authenticate" -Headers @{ "Content-Type" = "application/json" } -Body $xblBody).Token
+
+    $xstsBody = @{
         Properties = @{
-            SandboxId="RETAIL"
-            UserTokens=@($xblToken)
+            SandboxId  = "RETAIL"
+            UserTokens = @($xblToken)
         }
-        RelyingParty="rp://api.minecraftservices.com/"
-        TokenType="JWT"
-    } | ConvertTo-Json)
-    $identity = "XBL3.0 x=$($xsts.DisplayClaims.xui[0].uhs);$($xsts.Token)"
+        RelyingParty = "rp://api.minecraftservices.com/"
+        TokenType    = "JWT"
+    } | ConvertTo-Json
 
-    $token = (Invoke-RestMethod -Method Post -Uri "https://api.minecraftservices.com/authentication/login_with_xbox" -Headers @{ "Content-Type"="application/json" } -Body (@{identityToken=$identity} | ConvertTo-Json)).access_token
-    $profile = Invoke-RestMethod "https://api.minecraftservices.com/minecraft/profile" -Headers @{Authorization="Bearer $token"}
+    $xstsResponse = Invoke-RestMethod -Method Post -Uri "https://xsts.auth.xboxlive.com/xsts/authorize" -Headers @{ "Content-Type" = "application/json" } -Body $xstsBody
 
-    Set-Content $loginFile -Value (@{token=$token; profile=$profile} | ConvertTo-Json -Depth 6)
+    $identity = "XBL3.0 x=$($xstsResponse.DisplayClaims.xui[0].uhs);$($xstsResponse.Token)"
+
+    $mcTokenResponse = Invoke-RestMethod -Method Post -Uri "https://api.minecraftservices.com/authentication/login_with_xbox" -Headers @{ "Content-Type" = "application/json" } -Body (@{ identityToken = $identity } | ConvertTo-Json)
+
+    $mcToken = $mcTokenResponse.access_token
+
+    $profile = Invoke-RestMethod "https://api.minecraftservices.com/minecraft/profile" -Headers @{ Authorization = "Bearer $mcToken" }
+
+    Set-Content $loginFile -Value (@{ token = $mcToken; profile = $profile } | ConvertTo-Json -Depth 6)
 }
