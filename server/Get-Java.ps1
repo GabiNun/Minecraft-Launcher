d7d5b97f9e00574c6772b58ad4f48103a1cf5f0e
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-16 17:00:03 +0300

    Update Get-Java.ps1

--- server/Get-Java.ps1
+++ server/Get-Java.ps1
@@ -1,4 +1,5 @@
 if (-not (gcm java -Ea 0)) {
     Write-Host "Java not found. Installing Oracle JDK 21..."
     winget install --id Oracle.JDK.21 -e
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine")
 }
