3d04a82abc71092a1e457932bdab89f3ab870b3f
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-17 21:02:40 +0300

    Update Test.ps1

--- server/Test.ps1
+++ server/Test.ps1
@@ -1,6 +1,6 @@
 $ProgressPreference = 'SilentlyContinue'
 
-$json = Invoke-RestMethod -Uri "https://piston-meta.mojang.com/v1/packages/db4d7600e0d402a7ba7ad16ce748098f4c704d75/1.21.8.json"
+$json = Invoke-RestMethod "https://piston-meta.mojang.com/v1/packages/db4d7600e0d402a7ba7ad16ce748098f4c704d75/1.21.8.json"
 foreach ($lib in $json.libraries) {
     if ($null -ne $lib.downloads.artifact -and $lib.downloads.artifact.url -and $lib.downloads.artifact.path) {
         $dest = Join-Path -Path "libraries" -ChildPath $lib.downloads.artifact.path
@@ -8,6 +8,6 @@ foreach ($lib in $json.libraries) {
         if (-not (Test-Path $folder)) {
             New-Item -ItemType Directory -Force -Path $folder | Out-Null
         }
-        Invoke-WebRequest -Uri $lib.downloads.artifact.url -OutFile $dest
+        Invoke-WebRequest $lib.downloads.artifact.url -OutFile $dest
     }
 }
