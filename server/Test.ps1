da9a3cb06ad90df7b9236d60a8a5239f70d379d4
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-17 22:58:53 +0300

    Update Test.ps1

--- server/Test.ps1
+++ server/Test.ps1
@@ -1,7 +1,7 @@
 $ProgressPreference = 'SilentlyContinue'
 
 if (-not (Test-Path "$env:APPDATA\.minecraft")) {
-    ni $env:APPDATA\.minecraft\assets\indexes -ItemType Directory | Out-Null
+    ni $env:APPDATA\.minecraft\assets\indexes -I D | Out-Null
 }
 Set-Location $env:APPDATA\.minecraft
 
