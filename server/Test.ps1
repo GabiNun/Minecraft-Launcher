fedc63ceb8113e3677a8ee2f37606d26f79f4220
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-17 20:56:58 +0300

    Create Test.ps1

---
+++ server/Test.ps1
@@ -0,0 +1,16 @@
+$json = Invoke-RestMethod -Uri piston-meta.mojang.com/v1/packages/db4d7600e0d402a7ba7ad16ce748098f4c704d75/1.21.8.json
+foreach ($lib in $json.libraries) {
+    $artifact = $lib.downloads.artifact
+    if ($null -ne $artifact) {
+        $url = $artifact.url
+        $path = $artifact.path
+        if ($url -and $path) {
+            $dest = Join-Path -Path "." -ChildPath $path
+            $folder = Split-Path $dest -Parent
+            if (-not (Test-Path $folder)) {
+                New-Item -ItemType Directory -Force -Path $folder | Out-Null
+            }
+            Invoke-WebRequest -Uri $url -OutFile $dest
+        }
+    }
+}
