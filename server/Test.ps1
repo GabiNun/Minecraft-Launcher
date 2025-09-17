30ef33a78c151c0de3842f9014b2104b1a6506b2
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-17 21:56:59 +0300

    Update Test.ps1

--- server/Test.ps1
+++ server/Test.ps1
@@ -11,6 +11,10 @@ if (Test-Path login.json) {
     irm raw.githubusercontent.com/GabiNun/Minecraft-Launcher/main/Microsoft-Login.ps1 | iex
 }
 
+if (-not (Test-Path "client.jar")) {
+    Invoke-WebRequest "https://piston-data.mojang.com/v1/objects/a19d9badbea944a4369fd0059e53bf7286597576/client.jar" -OutFile "client.jar"
+}
+
 $json = Invoke-RestMethod "https://piston-meta.mojang.com/v1/packages/db4d7600e0d402a7ba7ad16ce748098f4c704d75/1.21.8.json"
 foreach ($lib in $json.libraries) {
     if ($null -ne $lib.downloads.artifact -and $lib.downloads.artifact.url -and $lib.downloads.artifact.path) {
@@ -21,10 +25,6 @@ foreach ($lib in $json.libraries) {
         }
         Invoke-WebRequest $lib.downloads.artifact.url -OutFile $dest
     }
-}
-
-if (-not (Test-Path "client.jar")) {
-    Invoke-WebRequest "https://piston-data.mojang.com/v1/objects/a19d9badbea944a4369fd0059e53bf7286597576/client.jar" -OutFile "client.jar"
 }
 
 $classpathString = "$([string]::Join(';', (gci libraries -r -fi *.jar).FullName));client.jar"
