3cf884dd921199ab04476b151fa70f045c846bbe
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-17 21:44:00 +0300

    Update Microsoft-Login.ps1

--- Microsoft-Login.ps1
+++ Microsoft-Login.ps1
@@ -33,5 +33,11 @@ function Get-Microsoft-Minecraft-Identity {
     $token = (Invoke-RestMethod -Method Post -Uri "https://api.minecraftservices.com/authentication/login_with_xbox" -Headers @{ "Content-Type"="application/json" } -Body (@{identityToken=$identity} | ConvertTo-Json)).access_token
     $profile = Invoke-RestMethod "https://api.minecraftservices.com/minecraft/profile" -Headers @{Authorization="Bearer $token"}
 
-    Set-Content "$env:APPDATA\.minecraft\login.json" -Value (@{token=$token; profile=$profile} | ConvertTo-Json -Depth 6)
+    $loginFile = "$env:APPDATA\.minecraft\login.json"
+    ni $loginFile | Out-Null
+
+    Set-Content $loginFile -Value (@{token=$token; profile=$profile} | ConvertTo-Json -Depth 6)
 }
+
+Get-Microsoft-Minecraft-Identity
+$login = Get-Content login.json -Raw | ConvertFrom-Json
