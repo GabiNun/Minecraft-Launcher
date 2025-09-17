8bb3d2e65decf8b355837c804e09b40b0e830218
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-17 22:44:51 +0300

    Update Test.ps1

--- server/Test.ps1
+++ server/Test.ps1
@@ -15,6 +15,7 @@ if (-not (Test-Path "client.jar")) {
     Invoke-WebRequest "https://piston-data.mojang.com/v1/objects/a19d9badbea944a4369fd0059e53bf7286597576/client.jar" -OutFile "client.jar"
 }
 
+$assetIndex = Invoke-RestMethod "https://piston-meta.mojang.com/v1/packages/7db0407a8e9e9a0520b5e3ecba3a3e4650169cd6/26.json"
 $json = Invoke-RestMethod "https://piston-meta.mojang.com/v1/packages/db4d7600e0d402a7ba7ad16ce748098f4c704d75/1.21.8.json"
 foreach ($lib in $json.libraries) {
     if ($null -ne $lib.downloads.artifact -and $lib.downloads.artifact.url -and $lib.downloads.artifact.path) {
@@ -27,9 +28,7 @@ foreach ($lib in $json.libraries) {
     }
 }
 
-$assetIndex = Invoke-RestMethod -Uri "https://piston-meta.mojang.com/v1/packages/7db0407a8e9e9a0520b5e3ecba3a3e4650169cd6/26.json"
-
-foreach ($entry in $assetIndex.objects.GetEnumerator()) {
+foreach ($entry in $assetIndex.objects.PSObject.Properties) {
     if (-not (Test-Path ("assets\objects\" + $entry.Value.hash.Substring(0,2)))) {
         New-Item -ItemType Directory -Force -Path ("assets\objects\" + $entry.Value.hash.Substring(0,2)) | Out-Null
     }
