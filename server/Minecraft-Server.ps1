7563945d636ce2f788d01bf79af0c70d8ffb9302
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-19 12:46:24 +0300

    Update Minecraft-Server.ps1

--- server/Minecraft-Server.ps1
+++ server/Minecraft-Server.ps1
@@ -1,5 +1,4 @@
 irm raw.githubusercontent.com/GabiNun/Minecraft-Launcher/main/server/Get-Java.ps1 | iex
-$ProgressPreference = 'SilentlyContinue'
 
 if (-not (Test-Path "$env:APPDATA\Minecraft Server")) {
     ni "$env:APPDATA\Minecraft Server" -I D | Out-Null
@@ -11,6 +10,7 @@ if (-not (Test-Path eula.txt)) {
 }
 
 if (-not (Test-Path server.jar)) {
+    $ProgressPreference = 'SilentlyContinue'
     irm piston-data.mojang.com/v1/objects/6bce4ef400e4efaa63a13d5e6f6b500be969ef81/server.jar -o server.jar
 }
 
