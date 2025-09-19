8256008bec1225928e12d93f75b52116ca203bc0
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-19 12:35:53 +0300

    Update Test.ps1

--- server/Test.ps1
+++ server/Test.ps1
@@ -6,20 +6,45 @@ $clientId = "66cf6e5a-b98b-4e05-b2bd-568d6e30828e"
 $clientSecret = "BpC8Q~FdVflzQWMpaP16MS1VaoH5_4lEsgWrtaxP"  # Your client secret
 $redirectUri = "http://localhost"  # Your redirect URI
 
+# Function to get an authorization code
+function Get-AuthorizationCode {
+    param (
+        [string]$tenantId,
+        [string]$clientId,
+        [string]$redirectUri
+    )
+
+    $authorizeUri = "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/authorize?"
+    $authorizeUri += "client_id=$clientId"
+    $authorizeUri += "&response_type=code"
+    $authorizeUri += "&redirect_uri=$redirectUri"
+    $authorizeUri += "&response_mode=query"
+    $authorizeUri += "&scope=https://graph.microsoft.com/.default"
+    $authorizeUri += "https://api.minecraftservices.com/minecraft/profile"
+
+    Start-Process $authorizeUri
+    Write-Output "Please authorize the application and copy the full redirect URI from your browser."
+    $redirectUriWithCode = Read-Host "Paste the full redirect URI here"
+    return $redirectUriWithCode
+}
+
 # Function to get an access token
 function Get-AccessToken {
     param (
         [string]$tenantId,
         [string]$clientId,
         [string]$clientSecret,
-        [string]$redirectUri
+        [string]$redirectUri,
+        [string]$authorizationCode
     )
 
     $body = @{
         client_id     = $clientId
-        scope         = "https://graph.microsoft.com/.default"
+        scope         = "https://graph.microsoft.com/.default https://api.minecraftservices.com/minecraft/profile"
         client_secret = $clientSecret
-        grant_type    = "client_credentials"
+        grant_type    = "authorization_code"
+        code          = $authorizationCode
+        redirect_uri  = $redirectUri
     }
 
     $response = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token" -Method Post -Body $body
@@ -63,7 +88,8 @@ function Get-MinecraftInformation {
 }
 
 # Main script
-$accessToken = Get-AccessToken -tenantId $tenantId -clientId $clientId -clientSecret $clientSecret -redirectUri $redirectUri
+$authorizationCode = Get-AuthorizationCode -tenantId $tenantId -clientId $clientId -redirectUri $redirectUri
+$accessToken = Get-AccessToken -tenantId $tenantId -clientId $clientId -clientSecret $clientSecret -redirectUri $redirectUri -authorizationCode $authorizationCode
 
 if ($accessToken) {
     $userInfo = Get-UserInformation -accessToken $accessToken
