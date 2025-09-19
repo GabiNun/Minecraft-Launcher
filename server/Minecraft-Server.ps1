61ec3e15567e07ea24c8cfe3f4fb4767abd53b09
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-19 12:41:09 +0300

    Update Minecraft-Server.ps1

--- server/Minecraft-Server.ps1
+++ server/Minecraft-Server.ps1
@@ -6,7 +6,10 @@ if (-not (Test-Path "$env:APPDATA\Minecraft Server")) {
 
 Set-Location "$env:APPDATA\Minecraft Server"
 
-Set-Content "$env:APPDATA\Minecraft Server\eula.txt" eula=true
+if (-not (Test-Path "$env:APPDATA\Minecraft Server\eula.txt")) {
+    Set-Content "$env:APPDATA\Minecraft Server\eula.txt" eula=true
+}
+
 $ProgressPreference = 'SilentlyContinue'
 
 $manifest = irm launchermeta.mojang.com/mc/game/version_manifest.json
