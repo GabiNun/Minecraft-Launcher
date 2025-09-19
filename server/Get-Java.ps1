e8b2cc3b8aa7f8700006e7d90a6be43427a6e4bd
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-19 13:16:24 +0300

    Update Get-Java.ps1

--- server/Get-Java.ps1
+++ server/Get-Java.ps1
@@ -1,5 +1,5 @@
 if (-not (gcm java -Ea 0)) {
-    Write-Host "Java not found. Installing Oracle JDK 24..."
+    Write-Host "Java not found. Installing Java 24"
     winget install --id Oracle.JDK.24 -e
     $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine")
 }
