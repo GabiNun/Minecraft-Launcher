dd8e78d8bd94d5d4e13ac5ba460587479338b664
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-19 13:06:47 +0300

    Update Minecraft-Server.ps1

--- server/Minecraft-Server.ps1
+++ server/Minecraft-Server.ps1
@@ -10,9 +10,7 @@ if (-not (Test-Path eula.txt)) {
 }
 
 if (-not (Test-Path server.jar)) {
-    $ProgressPreference = 'SilentlyContinue'
-    irm piston-data.mojang.com/v1/objects/6bce4ef400e4efaa63a13d5e6f6b500be969ef81/server.jar -o server.jar
+    curl.exe -s -o server.jar piston-data.mojang.com/v1/objects/6bce4ef400e4efaa63a13d5e6f6b500be969ef81/server.jar
 }
 
 & java -jar server.jar nogui
-#curl.exe -s -o server.jar https://piston-data.mojang.com/v1/objects/6bce4ef400e4efaa63a13d5e6f6b500be969ef81/server.jar
