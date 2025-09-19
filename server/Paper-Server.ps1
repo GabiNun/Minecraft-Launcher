a1883a955b906a21a691ae010640eb7cbf83f814
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-19 13:47:09 +0300

    Update Paper-Server.ps1

--- server/Paper-Server.ps1
+++ server/Paper-Server.ps1
@@ -14,5 +14,4 @@ if (-not (Test-Path server.jar)) {
     irm fill-data.papermc.io/v1/objects/8de7c52c3b02403503d16fac58003f1efef7dd7a0256786843927fa92ee57f1e/paper-1.21.8-60.jar -o server.jar
 }
 
-
 & java -jar server.jar nogui
