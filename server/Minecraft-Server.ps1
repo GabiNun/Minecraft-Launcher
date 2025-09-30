835e41060cf9a7053c58cbdbea3a2853d6724db6
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-30 22:20:15 +0300

    Update Minecraft-Server.ps1

--- server/Minecraft-Server.ps1
+++ server/Minecraft-Server.ps1
@@ -8,7 +8,7 @@ if (-not (Test-Path "$env:APPDATA\Minecraft Server")) {
 
 if (-not (Test-Path server.jar)) {
     $ProgressPreference = 'SilentlyContinue'
-    irm piston-data.mojang.com/v1/objects/6bce4ef400e4efaa63a13d5e6f6b500be969ef81/server.jar -o server.jar
+    irm piston-data.mojang.com/v1/objects/11e54c2081420a4d49db3007e66c80a22579ff2a/server.jar -o server.jar
 }
 
 & java -jar server.jar nogui
