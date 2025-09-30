4349d13d987df2ae06671c7bb1ac78ea2f80c44b
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-09-30 22:20:48 +0300

    Update Paper-Server.ps1

--- server/Paper-Server.ps1
+++ server/Paper-Server.ps1
@@ -8,7 +8,7 @@ if (-not (Test-Path "$env:APPDATA\Minecraft Server")) {
 
 if (-not (Test-Path server.jar)) {
     $ProgressPreference = 'SilentlyContinue'
-    irm fill-data.papermc.io/v1/objects/8de7c52c3b02403503d16fac58003f1efef7dd7a0256786843927fa92ee57f1e/paper-1.21.8-60.jar -o server.jar
+    irm fill-data.papermc.io/v1/objects/9a51088d04ecf56da32834335271122011e3c58d3bf24d8007f476071e390602/paper-1.21.9-37.jar -o server.jar
 }
 
 & java -jar server.jar nogui
