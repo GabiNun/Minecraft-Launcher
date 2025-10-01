14ebd6e2771d906f5c2a52d124f7931a1183a12c
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-01 05:02:30 -0700

    Update Minecraft-Server.ps1

--- server/Minecraft-Server.ps1
+++ server/Minecraft-Server.ps1
@@ -7,7 +7,7 @@ if (-not (Test-Path "$env:APPDATA\Minecraft Server")) {
 
 if (-not (Test-Path server.jar)) {
     $ProgressPreference = 'SilentlyContinue'
-    irm piston-data.mojang.com/v1/objects/11e54c2081420a4d49db3007e66c80a22579ff2a/server.jar -o server.jar
+    irm piston-data.mojang.com/v1/objects/11e54c2081420a4d49db3007e66c80a22579ff2a/server.jar -OutFile server.jar
 }
 
 java --enable-native-access=ALL-UNNAMED -jar server.jar nogui
