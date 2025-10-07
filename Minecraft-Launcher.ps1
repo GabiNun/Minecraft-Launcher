fcd66947ee75101a117bd01c6292d71f9a31f637
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-07 15:10:57 -0700

    Update Minecraft-Launcher.ps1

--- Minecraft-Launcher.ps1
+++ Minecraft-Launcher.ps1
@@ -4,7 +4,6 @@ $ProgressPreference = 'SilentlyContinue'
 if (-not (Test-Path $env:APPDATA\.minecraft)) {
     New-Item -ItemType Directory $env:APPDATA\.minecraft\assets\indexes | Out-Null
 }
-
 Set-Location $env:APPDATA\.minecraft
 
 irm raw.githubusercontent.com/GabiNun/Minecraft-Launcher/main/Microsoft-Login.ps1 | iex
