edf02acad7daa8e3d79b6d6f2404ae081ebcc829
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-12 11:02:11 -0700

    Update Version.ps1

--- server/Version.ps1
+++ server/Version.ps1
@@ -1,2 +1,3 @@
 $release = (Invoke-RestMethod 'https://launchermeta.mojang.com/mc/game/version_manifest.json').versions | ? type -eq 'release' | select -First 1 | % url
+
 $client = (Invoke-RestMethod $release).downloads.client.url
