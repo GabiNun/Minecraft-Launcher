3faa909014a30be5b0e22a71fb2dc52ff885885d
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-20 00:06:33 -0700

    Update Paper-Server.ps1

--- server/Paper-Server.ps1
+++ server/Paper-Server.ps1
@@ -4,7 +4,7 @@ Set-Location "$env:APPDATA\Minecraft Server"
 
 if (-not (Test-Path server.jar)) {
     $ProgressPreference = 0
-    irm fill-data.papermc.io/v1/objects/4bfb3447484e7daaacf595158ab4031058fba47be619869b81b3f5c9e6ca9ac2/paper-1.21.10-82.jar -Out server
+    irm fill-data.papermc.io/v1/objects/461bf33493781600a2163b1ee84237b2cac5401d5e1c70801742a617e51df26c/paper-1.21.10-84.jar -Out server
 }
 
 java -jar server nogui
